6f089d0f0ffe01286b4f29a234fca80fe6b26850
Amol Shirke <shirkeamy@gmail.com>
2025-02-07 19:13:20 +0530

    Add Modal class

---
+++ crud-net-core/Model/LoginMaster.cs
@@ -0,0 +1,18 @@
+﻿namespace crud_net_core.Model
+{
+    public class LoginMaster
+    {
+        public int LoginId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+
+    public class LoginBody
+    {
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+    }
+
+}
