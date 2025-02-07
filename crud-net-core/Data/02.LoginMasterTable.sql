1ffb9fe4838998951c9fd3cc88030d25fffb1d5e
Amol Shirke <shirkeamy@gmail.com>
2025-02-07 18:41:43 +0530

    insert dummy data into login master table

--- crud-net-core/Data/02.LoginMasterTable.sql
+++ crud-net-core/Data/02.LoginMasterTable.sql
@@ -5,3 +5,9 @@
 	CreatedDate datetime2 default(GETDATE()),
 	CreatedBy varchar(10) default('SYS Admin')
 )
+GO
+
+insert into LoginMaster (UserName, UserPassword) values
+('sam@email.com', 'password'),
+('john@example.com', 'StrongPassword'),
+('joe@email.com', 'Y0urStr0ngPass0rd')
