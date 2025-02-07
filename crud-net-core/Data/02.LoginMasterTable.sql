efbff470c4fc32f1fed541e9f21e9d724535adab
Amol Shirke <shirkeamy@gmail.com>
2025-02-07 18:39:05 +0530

    Add LoginMaster table script

---
+++ crud-net-core/Data/02.LoginMasterTable.sql
@@ -0,0 +1,7 @@
+﻿create table LoginMaster (
+	LoginId int primary key identity(1,1),
+	UserName nvarchar(50) unique,
+	UserPassword nvarchar(20) unique,
+	CreatedDate datetime2 default(GETDATE()),
+	CreatedBy varchar(10) default('SYS Admin')
+)
